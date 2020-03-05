193d9badeb40f2e4d5953d21b7a10bc33dbb6d07
Dynastiy <emmanuel00michael@gmail.com>
2020-03-05 22:46:14 +0100

    added files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+// Vue.use(require('vue-moment'));
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  components: { App },
+  template: '<App/>'
 })
